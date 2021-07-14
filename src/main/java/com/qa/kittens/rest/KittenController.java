09951c91ef23de2f0b7ee719cf3f7aeb8d4055b5
jordan <jharrison1@qa.com>
2021-07-14 14:21:47 +0100

    finished CRUD example

--- src/main/java/com/qa/kittens/rest/KittenController.java
+++ src/main/java/com/qa/kittens/rest/KittenController.java
@@ -1,9 +1,13 @@
 package com.qa.kittens.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,8 @@ import com.qa.kittens.data.Kitten;
 
 @RestController
 public class KittenController {
+
+	private List<Kitten> kittens = new ArrayList<>();
 
 	@GetMapping("/") // MAPS a GET request to "/" to this method
 	public String hello() {
@@ -21,15 +27,29 @@ public class KittenController {
 	public void createKitten(@RequestBody Kitten kitten) { // less fancy
 		// just Java
 		System.out.println(kitten);
+		this.kittens.add(kitten);
 	}
 
-	@GetMapping("/getKitten")
-	public Kitten getKitten() {
-		return new Kitten("Tabby", "Maine Coon", 12, 9);
+	@GetMapping("/getAllKittens")
+	public List<Kitten> getAllKittens() {
+		return this.kittens;
+	}
+
+	@GetMapping("/getKitten/{id}")
+	public Kitten getKitten(@PathVariable int id) {
+		Kitten found = this.kittens.get(id);
+		return found;
+	}
+
+	@PutMapping("/replaceKitten/{id}")
+	public Kitten replaceKitten(@PathVariable int id, @RequestBody Kitten newKitten) {
+		return this.kittens.set(id, newKitten); // replace the kitten at index id
 	}
 
 	@DeleteMapping("/deleteKitten/{id}")
 	public String deleteKitten(@PathVariable int id) {
+		this.kittens.remove(id);
+
 		return "Deleted kitten at index: " + id;
 	}
 
