542baba78fe1f1b114641dfc837bd842765d07c7
jordan <jharrison1@qa.com>
2021-07-14 10:41:28 +0100

    added CRUD to controller example

--- src/main/java/com/qa/kittens/rest/KittenController.java
+++ src/main/java/com/qa/kittens/rest/KittenController.java
@@ -1,7 +1,13 @@
 package com.qa.kittens.rest;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.kittens.data.Kitten;
 
 @RestController
 public class KittenController {
@@ -10,4 +16,21 @@ public class KittenController {
 	public String hello() {
 		return "Hello, World!";
 	}
+
+	@PostMapping("/createKitten") // fancy
+	public void createKitten(@RequestBody Kitten kitten) { // less fancy
+		// just Java
+		System.out.println(kitten);
+	}
+
+	@GetMapping("/getKitten")
+	public Kitten getKitten() {
+		return new Kitten("Tabby", "Maine Coon", 12, 9);
+	}
+
+	@DeleteMapping("/deleteKitten/{id}")
+	public String deleteKitten(@PathVariable int id) {
+		return "Deleted kitten at index: " + id;
+	}
+
 }
